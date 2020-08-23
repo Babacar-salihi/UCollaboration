3cd7e8f5934ca77f9565c5e02d5891b02a85748b
Babacar Ndong <ndongbabacar708@gmail.com>
2020-08-23 21:13:02 +0000

    Gérer compte utilisateur

---
+++ app/src/main/java/com/babacar/ucollaboration/Globals/Activitys/GererCompte.java
@@ -0,0 +1,16 @@
+package com.babacar.ucollaboration.Globals.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.babacar.ucollaboration.R;
+
+public class GererCompte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gerer_compte);
+    }
+}
